55d76519b9b6cf05e3fe1103b4783d3a83533a1a
Daniel Marques <daniel.marques142@gmail.com>
2020-02-17 15:01:58 -0300

    questao4Solved

--- Questao4.py
+++ Questao4.py
@@ -11,16 +11,14 @@ print(A)
 
 # Funções / Procedimentos
 def mdc(A, B):
-    while A != '-1' or B != '-1':
+    while A != -1 or B != -1:
         while B != 0:
             resto = A % B
             A = B
             B = resto
         print(A)
         A, B = map(int, input().split())
-        if A < 1 and B < 1:
-            break
-    mdc(A, B) # Chamada Recursiva da função/ Procedimento
+
 
 # Programa principal
 A, B = map(int, input().split())
